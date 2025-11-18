42d023c798061caa0888c8743758743761137bd9
ANightDazingZoroark <anightdazingzoroark22@gmail.com>
2025-11-18 20:51:26 +0800

    easier data transfer into UIs!!!

---
+++ src/main/java/anightdazingzoroark/riftlib/ui/RiftLibUIData.java
@@ -0,0 +1,57 @@
+package anightdazingzoroark.riftlib.ui;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+
+//another helper class, this one allows for quick transfer of data
+//and conversion into nbt to transfer into a UI
+public class RiftLibUIData {
+    private final NBTTagCompound nbtTagCompound = new NBTTagCompound();
+
+    public RiftLibUIData addInteger(String key, int data) {
+        this.nbtTagCompound.setInteger(key, data);
+        return this;
+    }
+
+    public RiftLibUIData addDouble(String key, double data) {
+        this.nbtTagCompound.setDouble(key, data);
+        return this;
+    }
+
+    public RiftLibUIData addFloat(String key, float data) {
+        this.nbtTagCompound.setFloat(key, data);
+        return this;
+    }
+
+    public RiftLibUIData addBoolean(String key, boolean data) {
+        this.nbtTagCompound.setBoolean(key, data);
+        return this;
+    }
+
+    public RiftLibUIData addString(String key, String data) {
+        this.nbtTagCompound.setString(key, data);
+        return this;
+    }
+
+    public RiftLibUIData addEntity(String key, Entity entity) {
+        NBTTagCompound dataToAdd = new NBTTagCompound();
+        entity.writeToNBT(dataToAdd);
+        this.nbtTagCompound.setTag(key, dataToAdd);
+        return this;
+    }
+
+    public RiftLibUIData addNBTTagCompound(String key, NBTTagCompound data) {
+        this.nbtTagCompound.setTag(key, data);
+        return this;
+    }
+
+    //this combines an nbt tag compound with the one thats currently stored
+    public RiftLibUIData parseNBT(NBTTagCompound nbtTagCompound) {
+        this.nbtTagCompound.merge(nbtTagCompound);
+        return this;
+    }
+
+    public NBTTagCompound getFinalNBT() {
+        return this.nbtTagCompound;
+    }
+}
--- src/main/java/anightdazingzoroark/riftlib/ui/RiftLibUIHelper.java
+++ src/main/java/anightdazingzoroark/riftlib/ui/RiftLibUIHelper.java
@@ -14,6 +14,10 @@ public class RiftLibUIHelper {
         showUI(player, id, new NBTTagCompound(), x, y, z);
     }
 
+    public static void showUI(EntityPlayer player, String id, RiftLibUIData data, int x, int y, int z) {
+        showUI(player, id, data.getFinalNBT(), x, y, z);
+    }
+
     public static void showUI(EntityPlayer player, String id, NBTTagCompound nbtTagCompound, int x, int y, int z) {
         if (player instanceof EntityPlayerMP) ServerProxy.MESSAGE_WRAPPER.sendTo(new RiftLibOpenUI(id, nbtTagCompound, x, y, z), (EntityPlayerMP) (player));
         else ClientProxy.showUI(id, nbtTagCompound, x, y, z);

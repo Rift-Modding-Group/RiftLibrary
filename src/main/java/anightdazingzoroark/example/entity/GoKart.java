8fec5dffdb137967078df1017a724c5685f61a05
ANightDazingZoroark <anightdazingzoroark22@gmail.com>
2026-01-14 18:20:26 +0800

    go-kart example riding and controlling

--- src/main/java/anightdazingzoroark/example/entity/GoKart.java
+++ src/main/java/anightdazingzoroark/example/entity/GoKart.java
@@ -1,10 +1,20 @@
 package anightdazingzoroark.example.entity;
 
 import anightdazingzoroark.riftlib.core.IAnimatable;
+import anightdazingzoroark.riftlib.core.PlayState;
+import anightdazingzoroark.riftlib.core.builder.AnimationBuilder;
+import anightdazingzoroark.riftlib.core.builder.LoopType;
+import anightdazingzoroark.riftlib.core.controller.AnimationController;
+import anightdazingzoroark.riftlib.core.event.AnimationEvent;
 import anightdazingzoroark.riftlib.core.manager.AnimationData;
 import anightdazingzoroark.riftlib.core.manager.AnimationFactory;
-import net.minecraft.entity.EntityCreature;
+import anightdazingzoroark.riftlib.ridePositionLogic.IDynamicRideUser;
+import net.minecraft.entity.*;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class GoKart extends EntityCreature implements IAnimatable {
     private final AnimationFactory factory = new AnimationFactory(this);
@@ -14,8 +24,84 @@ public class GoKart extends EntityCreature implements IAnimatable {
     }
 
     @Override
-    public void registerControllers(AnimationData data) {
+    public boolean processInteract(EntityPlayer player, EnumHand hand) {
+        if (!player.isRiding()) {
+            this.getNavigator().clearPath();
+            player.startRiding(this, true);
+        }
+        return super.processInteract(player, hand);
+    }
 
+    //ride management stuff starts here
+    @Override
+    public void updatePassenger(Entity passenger) {
+        if (!this.isPassenger(passenger)) return;
+
+        this.rotationYaw = passenger.rotationYaw;
+        this.prevRotationYaw = this.rotationYaw;
+        this.rotationPitch = passenger.rotationPitch * 0.5f;
+        this.setRotation(this.rotationYaw, this.rotationPitch);
+        this.renderYawOffset = this.rotationYaw;
+
+        passenger.setPosition(this.posX, this.posY - 0.25D, this.posZ);
+    }
+
+    @Override
+    @Nullable
+    public Entity getControllingPassenger() {
+        for (Entity passenger : this.getPassengers()) {
+            if (passenger instanceof EntityPlayer) return passenger;
+        }
+        return null;
+    }
+
+    @Override
+    public void travel(float strafe, float vertical, float forward) {
+        if (this.isBeingRidden()) {
+            EntityLivingBase controller = (EntityLivingBase)this.getControllingPassenger();
+            if (controller != null) {
+                if (this.getAttackTarget() != null) {
+                    this.setAttackTarget(null);
+                    this.getNavigator().clearPath();
+                }
+
+                strafe = controller.moveStrafing * 0.5f;
+                forward = controller.moveForward;
+
+                if (forward <= 0.0F) forward *= 0.25F;
+
+                //movement
+                this.stepHeight = 1.0F;
+                this.jumpMovementFactor = this.getAIMoveSpeed() * 0.1F;
+                this.fallDistance = 0;
+                float riderSpeed = (float) (controller.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue());
+                float moveSpeed = (float)Math.max(0, this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue() - riderSpeed);
+                this.setAIMoveSpeed(moveSpeed);
+
+                super.travel(strafe, vertical, forward);
+            }
+        }
+        else {
+            this.stepHeight = 0.5F;
+            this.jumpMovementFactor = 0.02F;
+            super.travel(strafe, vertical, forward);
+        }
+    }
+    //ride management stuff ends here
+
+    @Override
+    public void registerControllers(AnimationData data) {
+        data.addAnimationController(new AnimationController(this, "movement", 0, new AnimationController.IAnimationPredicate() {
+            @Override
+            public PlayState test(AnimationEvent event) {
+                if (event.isMoving()) {
+                    event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.go_kart.move", LoopType.LOOP));
+                    return PlayState.CONTINUE;
+                }
+                event.getController().clearAnimationCache();
+                return PlayState.STOP;
+            }
+        }));
     }
 
     @Override
